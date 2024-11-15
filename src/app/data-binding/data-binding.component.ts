8e57a51aca98911fc048e47079f6901285ca827e
Akash Khandekar <akhandekar@osius.com>
2024-11-15 15:29:18 +0530

    Practiced Data Binding Concepts

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+
+  //Interpolation
+
+  firstName:string = "Akash";
+  LastName:string = "Khandekar";
+
+  bgColor:string = "bgColor";
+
+  changeColor(){
+    this.bgColor="changeColor";
+  }
+
+}
